7ffe448b7326f1710b1d3e089633b61ae89b247c
Michiel Appelman <michiel@appelman.se>
2017-04-04 21:04:41 +0200

    Adds CloudMQTT support to directly send commands

--- somfy_controller/views.py
+++ somfy_controller/views.py
@@ -4,13 +4,27 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from google.cloud import datastore
+import paho.mqtt.publish as publish
+from django_somfy.settings import SETTINGS
 
 @login_required()
 @require_http_methods(["GET", "POST"])
 def index(request):
     ds = datastore.Client()
     if request.method == 'POST':
-        if 'name' in request.POST and 'state' in request.POST: 
+        if 'name' in request.POST and 'state' in request.POST:
+            # Send to MQTT
+            publish.single("somfy/" + request.POST['name'],
+                           payload=request.POST['state'],
+                           hostname=SETTINGS['mqtt_host'],
+                           port=SETTINGS['mqtt_port'],
+                           auth={
+                               'username': SETTINGS['mqtt_user'],
+                               'password': SETTINGS['mqtt_pass']
+                               }
+                          )
+
+            # Update GCP Datastore
             entity = ds.get(ds.key('Controller', request.POST['name']))
             entity['state'] = request.POST['state']
             entity['timestamp'] = datetime.datetime.utcnow()
